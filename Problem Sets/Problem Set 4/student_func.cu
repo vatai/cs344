ec4f95d74670cd967577ca04cc5237e33948dad5
Emil Vatai <emil.vatai@gmail.com>
2016-09-15 10:43:50 +0200

    Trying to figure out where my stuff dissapeared

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -42,9 +42,91 @@
 
  */
 
-__global__ void sort(){
-  extern __shared__ unsigned int *sdata;
-  unsigned int *data; data = (unsigned int*) sdata;
+
+__global__ void radsort(unsigned int* d_in,
+			unsigned int* d_inpos,
+			unsigned int* d_out,
+			unsigned int* d_outpos,
+			size_t N)
+{
+  extern __shared__ int edata[];
+  unsigned int* idata  = (unsigned int*) edata + 0*N;
+  unsigned int* din    = (unsigned int*) edata + 1*N;
+  unsigned int* dout   = (unsigned int*) edata + 2*N;
+  unsigned int* inpos  = (unsigned int*) edata + 3*N;
+  unsigned int* outpos = (unsigned int*) edata + 4*N;
+  unsigned int tid = threadIdx.x;
+
+  unsigned int last;
+  din[tid] = d_in[tid];
+  inpos[tid] = d_inpos[tid];
+  
+  for(unsigned int sortmask = 1; sortmask != 0; sortmask <<= 1){
+    /* First half: sum scan the elements with 0 at the sortmask */
+    /* fill idata[] */
+    idata[tid] = ( (din[tid] & sortmask) == 0) ? 1 : 0;
+    __syncthreads();
+
+    /* scan phase I */
+    for (size_t skip = 1; skip < N; skip<<=1 ) {
+      size_t mask = (skip<<1)-1;
+      if( ( tid & mask ) == mask ) idata[tid] += idata[tid - skip];
+      __syncthreads();
+    }
+    last = idata[N-1]; // save last
+    /* scan phase II */
+    idata[N-1] = 0;
+    for (size_t skip = N/2; skip > 0; skip>>=1 ) {
+      unsigned int mask = (skip<<1)-1;
+      if( ( tid & mask ) == mask) {
+	float tmp = idata[tid] + idata[tid - skip];
+	idata[tid - skip] = idata[tid];
+	idata[tid]=tmp;
+      }
+      __syncthreads();
+    }
+    /* save indices to dout[] */
+    if( (din[tid] & sortmask) == 0 ) {
+      dout[idata[tid]] = din[tid];
+      outpos[idata[tid]] = inpos[tid];
+    }
+    __syncthreads();
+
+    //////////////////////////////////// HALF /////////////////////////////
+    /* First half: sum scan the elements with 1 at the sortmask */
+    /* fill idata[] */
+    idata[tid] = ( (din[tid] & sortmask) == sortmask ) ? 1 : 0;
+    __syncthreads();
+
+    /* scan phase I */
+    for (size_t skip = 1; skip < N; skip<<=1 ) {
+      size_t mask = (skip<<1)-1;
+      if( ( tid & mask ) == mask ) idata[tid] += idata[tid - skip];
+      __syncthreads();
+    }
+    /* scan phase II */
+    idata[N-1] = last; // TODO try with 'last' instead of '0'
+    for (size_t skip = N/2; skip > 0; skip>>=1 ) {
+      unsigned int mask = (skip<<1)-1;
+      if( ( tid & mask ) == mask) {
+	float tmp = idata[tid] + idata[tid - skip];
+	idata[tid - skip] = idata[tid];
+	idata[tid]=tmp;
+      }
+      __syncthreads();
+    }
+    /* save indices to dout[] */
+    if( (din[tid] & sortmask) == sortmask ) {
+      dout[idata[tid]] = din[tid];
+      outpos[idata[tid]] = inpos[tid];
+    }
+    __syncthreads();
+
+    /* put dout[] to din[] as an input to the next sortmask */
+    din[tid]=dout[tid];
+  }
+  d_out[tid] = dout[tid];
+  d_outpos[tid] = outpos[tid];
 }
 
 
@@ -53,8 +135,15 @@ void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_outputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
-{ 
+{
+  std::cout << "HELLOOOO:"  << numElems << std::endl;
   //TODO
   //PUT YOUR SORT HERE
-  
+  /*
+    radsort<<<1,numElems,5*numElems*sizeof(int)>>>( d_inputVals,
+				    d_inputPos,
+				    d_outputVals,
+				    d_outputPos,
+				    numElems);
+  //*/
 }
